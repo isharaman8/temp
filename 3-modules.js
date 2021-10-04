c24eb2aa11ba1e43b3d95bd3e288eeed112ae0b3
isharaman8 <isharaman8@gmail.com>
2021-10-04 20:42:40 +0530

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,25 @@
+// const amount = 12;
+
+// // if (amount < 10) {
+// // 	console.log("small number");
+// // } else {
+// // 	console.log("large number");
+// // }
+
+// // console.log(`hey its my first node app!!!`);
+// console.log(`${__dirname}`);
+// let count = 0;
+// setInterval(() => {
+// 	console.log(count);
+// 	count++;
+// }, 2000);
+const names = require("./4-names");
+const sayHi = require("./5-utils");
+const data = require("./6-alternative-flavor");
+require("./7-mind-grenade");
+const { john, peter } = names;
+// sayHi("Susan");
+// sayHi(john);
+// sayHi(peter);
+// sayHi(data.singlePerson.name);
+// console.log(data);
---
+++ 5-utils.js
@@ -0,0 +1,6 @@
+const sayHi = (name) => {
+	console.log(`Hello there ${name}`);
+	4;
+};
+
+module.exports = sayHi;
